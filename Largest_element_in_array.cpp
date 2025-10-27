f88c3bf800d2c4646acef13e3fac92f74ab1e352
samhitha176 <srisamhitharayudu@gmail.com>
2025-10-27 14:10:06 +0530

    easy_Largest_element_in_array.cpp

--- Largest_element_in_array.cpp
+++ Largest_element_in_array.cpp
@@ -1,3 +1,28 @@
+/*
+QUESTION:-
+Given an array A[] of size n. The task is to find the largest element in it.
+
+Example:
+
+Input:
+n = 5
+A[] = {1, 8, 7, 56, 90}
+Output:
+90
+Explanation:
+The largest element of given array is 90
+*/
+
+/*
+APPROACH:-
+-> Intialize the maxi with starting element
+-> Traverse the entire array and update the maxi if the element is greater than maxi
+-> Finally, return the maxi
+*/
+
+// CODE:-
+
+
 int largest(int arr[], int n)
 {
     int maxi=arr[0];
